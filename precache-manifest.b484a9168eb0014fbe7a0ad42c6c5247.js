a82cce2ab1c244ad82916a7a9aafdd1c5a3c6d15
vule <vksnjankovic@gmail.com>
2019-10-22 11:43:40 +0200

    Updates

---
+++ precache-manifest.b484a9168eb0014fbe7a0ad42c6c5247.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5994c74727dc6b4c5523ee00e4808118",
+    "url": "/connect4/index.html"
+  },
+  {
+    "revision": "11b36d4d995b704e4a67",
+    "url": "/connect4/static/css/main.8e60b741.chunk.css"
+  },
+  {
+    "revision": "c060bfb1d91668b7f9c5",
+    "url": "/connect4/static/js/2.6b5872fe.chunk.js"
+  },
+  {
+    "revision": "11b36d4d995b704e4a67",
+    "url": "/connect4/static/js/main.6b94d69c.chunk.js"
+  },
+  {
+    "revision": "a288ffe77038f075ec24",
+    "url": "/connect4/static/js/runtime-main.e169811f.js"
+  }
+]);
